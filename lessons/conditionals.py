39cefdf03bffa21df8312645af2096af9fb8b9fb
Sarah Burton <swb01@live.unc.edu>
2023-01-19 23:24:11 -0500

    exercise 1 progress

---
+++ lessons/conditionals.py
@@ -0,0 +1,10 @@
+"""If it's raining, tell me to pack an umbrella"""
+
+weather: str = input("What is the weather like? ")
+
+if (weather == "rain"):
+    print("Pack an umbrella!")
+    print("But splash in the puddles!")
+else:
+    print("You don't need an umbrella!")
+print("Have a lovely day!!")
---
+++ lessons/conditionals2.py
@@ -0,0 +1,9 @@
+"""Interrupting Cow"""
+inter_cow: str = input("Do you want an interrupting cow? ")
+
+print("Knock knock.")
+if(inter_cow == "yes"):
+    print("...who's there?")
+    print("Interrupting cow.")
+    print("Interrupting cow who--")
+    print("MOOO")
